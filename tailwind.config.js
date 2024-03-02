ea10fd9f3eef97f88438391b0940d09c5d077b0e
Allan Cassio <allan.cassio1@gmail.com>
2024-03-02 07:16:34 -0300

    config tailwind and theme roboto in project

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "roboto": ["Roboto", 'sans-serif']
+      },
+      backgroundColor: {
+        "primay": "#FFFFFF",
+        "secondary": "#D9D9D9"
+      },
+      colors: {
+        "primary": "#00BDFF",
+        "primary-hover": "#00B0FF",
+        "button-positive-primary": "#5E17F5",
+        "button-positive-hover": "#5E17A5",
+        "button-negative-primary": "#FF4E3A",
+        "button-negative-hover": "#FF4011",
+        "button-cancel-primary": "#F2F2F2",
+        "button-cancel-hover": "#D9D9D9",
+      },
+    },
   },
   plugins: [],
 }
